f7c713a00978619f5826d4f041ec5002cec68b20
Your Name <kaitok857@gmail.com>
2023-10-26 17:11:39 +0700

    soal

--- phase 0/soal_part_11/soal2.js
+++ phase 0/soal_part_11/soal2.js
@@ -9,15 +9,32 @@ Jika input adalah string kosong ('') maka return array kosong
 */
 
 function meleeRangedGrouping (str) {
-    //your code here
+  //your code here
+  if (!str) {
+      return [];
   }
-  
-  // TEST CASE
-  
-  console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
-  // [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
-  
-  console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
-  // [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
-  
-  console.log(meleeRangedGrouping('')); // []
+  const hero = str.split(',')
+  // console.log (hero)
+  let classMelee = [];
+  let classRanged = [];
+
+  for (let heroes of hero) {
+      const [nama, tipe] = heroes.split('-')
+      // console.log ([nama, tipe])
+      if (tipe === 'Melee') {
+          classMelee.push(nama)
+      }else if (tipe === 'Ranged') {
+          classRanged.push(nama)
+      }
+  }
+  return [classRanged, classMelee]
+}
+// TEST CASE
+
+console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
+// [ ['Razor', 'Invoker', 'Sniper'], ['Meepo', 'Axe'] ]
+
+console.log(meleeRangedGrouping('Drow Ranger-Ranged,Chen-Ranged,Dazzle-Ranged,Io-Ranged'));
+// [ ['Drow Ranger', 'Chen', 'Dazzle', 'Io'], [] ]
+
+console.log(meleeRangedGrouping('')); // []
